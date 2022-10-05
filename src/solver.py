9d237976847f982f7145b289d9475c3026d5ff58
Aleksander Strzelecki <aleksander.strzelecki.2000@gmail.com>
2022-10-05 17:43:53 +0200

    rename symbol

--- src/solver.py
+++ src/solver.py
@@ -8,15 +8,15 @@ class Solver:
 
     @classmethod
     def solve_pair(cls, player, group):
-        player_idx = np.nonzero(player)[0]
-        group_idx = np.nonzero(group)[0]
-        if group_idx.size == 0:
-            return player_idx
+        player_tiles_idx = np.nonzero(player)[0]
+        group_tiles_idx = np.nonzero(group)[0]
+        if group_tiles_idx.size == 0:
+            return player_tiles_idx
         # 0 row colors
         # 1 row values
         # 2 row tile id
-        player_tiles = np.vstack([Rummikub.tiles[:, player_idx], player_idx])
-        group_tiles = np.vstack([Rummikub.tiles[:, group_idx], group_idx])
+        player_tiles = np.vstack([Rummikub.tiles[:, player_tiles_idx], player_tiles_idx])
+        group_tiles = np.vstack([Rummikub.tiles[:, group_tiles_idx], group_tiles_idx])
         result = np.array([], dtype=int)
 
         if np.all(group_tiles[0,:] == group_tiles[0,0]) and group_tiles.shape[1] < 13:
