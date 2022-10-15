42f1e0de9388d93993536eb0602565cd7cf35bf8
Aleksander Strzelecki <aleksander.strzelecki.2000@gmail.com>
2022-10-15 18:47:34 +0200

    add 101 move

--- src/monte_carlo.py
+++ src/monte_carlo.py
@@ -226,7 +226,7 @@ class MonteCarloTreeSearchNode():
         return current_node
 
     def best_action(self):
-        simulation_no = 100
+        simulation_no = 20
         
         
         for i in range(simulation_no):
