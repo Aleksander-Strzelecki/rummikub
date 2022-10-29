79818453da606c2ec8176656a8d64add7f785dc9
Aleksander Strzelecki <aleksander.strzelecki.2000@gmail.com>
2022-10-29 16:29:54 +0200

    add pytest

--- validation/solver/test_solver.py
+++ validation/solver/test_solver.py
@@ -1,15 +1,17 @@
 from solver import Solver
 import numpy as np
+import pytest
 
-solver = Solver()
-destination_group_tiles_with_idxs = np.array([[3,3,0],
-                                              [7,8,0],
-                                              [32,85,104]])
-source_group_with_idxs = np.array([[3],
-                                   [10],
-                                   [35]])
-no_joker_tiles_columns = np.array([0,1])                                
-condition = solver._get_rummikub_series_condition(destination_group_tiles_with_idxs, \
-    source_group_with_idxs, no_joker_tiles_columns)
+def test_get_rummikub_series_condition():
+    solver = Solver()
+    destination_group_tiles_with_idxs = np.array([[3,3,0],
+                                                [7,8,0],
+                                                [32,85,104]])
+    source_group_with_idxs = np.array([[3],
+                                    [10],
+                                    [35]])
+    no_joker_tiles_columns = np.array([0,1])                                
+    condition = solver._get_rummikub_series_condition(destination_group_tiles_with_idxs, \
+        source_group_with_idxs, no_joker_tiles_columns)
 
-assert condition == np.array([True])
+    assert condition == np.array([True])
