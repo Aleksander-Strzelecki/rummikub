af906ddfd25a11a31040a5209d8a9933c672ba0c
Aleksander Strzelecki <aleksander.strzelecki.2000@gmail.com>
2022-10-31 12:14:05 +0100

    fix tensorboard

--- src/callbacks/custom_tensorboard.py
+++ src/callbacks/custom_tensorboard.py
@@ -7,11 +7,13 @@ class CustomTensorboard(tf.keras.callbacks.Callback):
     logdir = "logs/scalars/" + datetime.now().strftime("%Y%m%d-%H%M%S")
     file_writer = tf.summary.create_file_writer(logdir + "/metrics")
     file_writer.set_as_default()
+    total_epoch = 0
 
     def on_epoch_end(self, epoch, logs=None):
-        tf.summary.scalar('loss', data=logs['loss'], step=epoch)
+        self.total_epoch += 1
+        tf.summary.scalar('loss', data=logs['loss'], step=self.total_epoch)
         if tbv.tensorboard_move_done:
-            tf.summary.scalar('loss_when_move', data=logs['loss'], step=epoch)
-            tf.summary.scalar('tiles_laid', data=tbv.tensorboard_tiles_laid, step=epoch)
+            tf.summary.scalar('loss_when_move', data=logs['loss'], step=self.total_epoch)
+            tf.summary.scalar('tiles_laid', data=tbv.tensorboard_tiles_laid, step=self.total_epoch)
         else:
-            tf.summary.scalar('loss_when_no_move', data=logs['loss'], step=epoch)
+            tf.summary.scalar('loss_when_no_move', data=logs['loss'], step=self.total_epoch)
