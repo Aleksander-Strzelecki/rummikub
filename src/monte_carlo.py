00dd2c7bcdf18ff50fd42bc52ea5b1ee9f59ccfd
Aleksander Strzelecki <aleksander.strzelecki.2000@gmail.com>
2022-10-31 17:32:11 +0100

    rare checkpoint

--- src/monte_carlo.py
+++ src/monte_carlo.py
@@ -386,7 +386,7 @@ class MonteCarloTreeSearchNode():
         cls.model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
             filepath=checkpoint_filepath,
             save_weights_only=True,
-            save_freq=10)
+            save_freq=100)
 
         cls.model_custom_tensorboard_callback = CustomTensorboard()
         
