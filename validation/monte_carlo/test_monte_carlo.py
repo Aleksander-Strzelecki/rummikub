3510de5d9ec467659098d7ccc0cb3002b124c315
Aleksander Strzelecki <aleksander.strzelecki.2000@gmail.com>
2022-11-02 16:43:53 +0100

    add next ut

--- validation/monte_carlo/test_monte_carlo.py
+++ validation/monte_carlo/test_monte_carlo.py
@@ -10,8 +10,15 @@ def test_best_action_from_rollout(tmp_path):
     monte_carlo.MonteCarloTreeSearchNode.create_models(str(tmp_path) + '/')
     root = monte_carlo.MonteCarloTreeSearchNode(state = mc_state)
 
-    mock_rollout_actions = [[[0,1,5],[0,1,2],[0,1,3],[0,1,4],[100,0,0]],
+    mock_rollout_actions1 = [[[0,1,5],[0,1,2],[0,1,3],[0,1,4],[100,0,0]],
                             [[0,1,1],[0,1,2],[0,1,3],[100,0,0]],
                             [[0,2,104],[0,2,1],[0,2,2],[0,2,3],[0,2,4],[0,2,5],[100,0,0]],
                             [[0,1,1],[2,1,2],[100,0,0]]]
-    assert root._best_action_from_rollout(mock_rollout_actions) == [[0,2,104],[0,2,1],[0,2,2],[0,2,3],[0,2,4],[0,2,5],[100,0,0]]
+
+    mock_rollout_actions2 = [[[0,1,5],[2,1,2],[3,1,3],[0,1,4],[100,0,0]],
+                            [[0,1,1],[0,1,2],[0,1,3],[100,0,0]],
+                            [[0,2,104],[1,2,1],[1,2,2],[1,2,3],[1,2,4],[1,2,5],[100,0,0]],
+                            [[0,1,1],[2,1,2],[100,0,0]]]
+
+    assert root._best_action_from_rollout(mock_rollout_actions1) == [[0,2,104],[0,2,1],[0,2,2],[0,2,3],[0,2,4],[0,2,5],[100,0,0]]
+    assert root._best_action_from_rollout(mock_rollout_actions2) == [[0,1,1],[0,1,2],[0,1,3],[100,0,0]]
