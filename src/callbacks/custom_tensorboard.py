4993a24a90b0a228d5a36a2cf8ef563cf6c39c84
Aleksander Strzelecki <aleksander.strzelecki.2000@gmail.com>
2022-11-15 20:47:52 +0100

    delete gd pathprefix from tensorboard

--- src/callbacks/custom_tensorboard.py
+++ src/callbacks/custom_tensorboard.py
@@ -4,7 +4,7 @@ from tensorflow import keras
 from datetime import datetime
 
 class CustomTensorboard(tf.keras.callbacks.Callback):
-    path_prefix = '/content/drive/MyDrive/rummikub/'
+    path_prefix = ''
     logdir = path_prefix + "logs/scalars/" + datetime.now().strftime("%Y%m%d-%H%M%S")
     file_writer = tf.summary.create_file_writer(logdir + "/metrics", max_queue=100)
     file_writer.set_as_default()
