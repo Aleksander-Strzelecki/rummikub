73109b1fab5ad40a9fbc2b1b6438da5805f2edf5
Aleksander Strzelecki <aleksander.strzelecki.2000@gmail.com>
2022-11-17 17:02:41 +0100

    change name of variable in tb

--- src/callbacks/custom_tensorboard.py
+++ src/callbacks/custom_tensorboard.py
@@ -14,7 +14,7 @@ class CustomTensorboard(tf.keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs=None):
         self.total_epoch += 1
         tf.summary.scalar('loss', data=logs['loss'], step=self.total_epoch)
-        tf.summary.scalar('tiles_laid', data=tbv.tensorboard_tiles_laid, step=self.total_epoch)
+        tf.summary.scalar('potential_tiles_laid_number', data=tbv.tensorboard_tiles_laid, step=self.total_epoch)
         for buffer_name in tbv.tensorboard_buffer_elements:
             tf.summary.scalar('elements in buffer_' + buffer_name, data=tbv.tensorboard_buffer_elements[buffer_name],
             step=self.total_epoch)
