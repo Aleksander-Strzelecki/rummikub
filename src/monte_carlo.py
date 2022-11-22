cbe38207be726aea175efcf91c748f92f59cb0ed
Aleksander Strzelecki <aleksander.strzelecki.2000@gmail.com>
2022-11-22 21:39:08 +0100

    constant iteration

--- src/monte_carlo.py
+++ src/monte_carlo.py
@@ -297,23 +297,21 @@ class MonteCarloTreeSearchNode():
             return self._groups_extended
 
     def best_actions(self, buffer:DataSet, positive_buffer:DataSet):
-        simulation_no=32
+        simulation_no=200
         actions = []
         spare_actions = []
 
-        while (spare_actions == []) and (simulation_no < 257):
-            for i in range(simulation_no):
-                #TODO save success actions from rollout
-                v = self._tree_policy()
-                reward, rollout_actions = v.rollout()
-                if rollout_actions:
-                    spare_actions.append(rollout_actions)
-                buffer, positive_buffer = v.backpropagate(reward, dataset=buffer, positive_dataset=positive_buffer)
-                buffer.shrink(self.BUFFER_SIZE)
-                positive_buffer.shrink(self.POSITIVE_BUFFER_SIZE)
-                buffer.tensorboard_update()
-                positive_buffer.tensorboard_update()
-            simulation_no = simulation_no * 2
+        for i in range(simulation_no):
+            #TODO save success actions from rollout
+            v = self._tree_policy()
+            reward, rollout_actions = v.rollout()
+            if rollout_actions:
+                spare_actions.append(rollout_actions)
+            buffer, positive_buffer = v.backpropagate(reward, dataset=buffer, positive_dataset=positive_buffer)
+            buffer.shrink(self.BUFFER_SIZE)
+            positive_buffer.shrink(self.POSITIVE_BUFFER_SIZE)
+            buffer.tensorboard_update()
+            positive_buffer.tensorboard_update()
         self._save_datasets([buffer, positive_buffer])
 
         child = self.best_child(c_param=0., ann_param=0., verbose=True)
